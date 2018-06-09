da89cce791b7041df423f4bfaaa037255c6c8186
tanchc <tanchc16@gmail.com>
2018-06-09 14:54:58 +0800

    Added Avalanche Particle System

---
+++ Assets/Temptest/TriggerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class TriggerScript : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // these lists are used to contain the particles which match
+    // the trigger conditions each frame.
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+    void OnEnable()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.startSize = 40.0f;
+        ps.startLifetime = 10;
+    }
+}
