db723096a583e1950e6fce08e2a3a54934f0e1e4
tanchc <tanchc16@gmail.com>
2018-06-13 18:05:22 +0800

    Added movement for player

---
+++ Assets/Scripts/ControllerInteractObject.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInteractObject : MonoBehaviour {
+
+    public GameObject player;
+    public float thrust;
+    private Rigidbody rb;
+    private SteamVR_TrackedObject trackedObj;
+
+    // 1
+    private GameObject collidingObject;
+    // 2
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+        if (collidingObject.name == "SwimController")
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Temptest/TestMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+    }
+}
