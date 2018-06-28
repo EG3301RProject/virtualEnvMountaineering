0f7c0ec075f605269d89d1adf711ea196b5db8cb
tanchc <tanchc16@gmail.com>
2018-06-28 12:46:06 +0800

    Added callback function for avalanche collision

---
+++ Assets/Scripts/AvalancheController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheController : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.name == "Player")
+        {
+            Rigidbody body = other.GetComponent<Rigidbody>();
+            body.AddForce(other.transform.forward * -50);
+        }
+    }
+}
