92e4189f18b3ba0181d329228e0101aae174b7aa
Topazed <righthandedpeople@gmail.com>
2018-07-22 14:08:23 +0800

    updated win/lose condition,UI,prompter

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeIn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Temp");
+        Debug.Log("Im clicked");
+    }
+
+    public void FadeMe()
+    {
+        StartCoroutine(Dofade());
+    }
+
+    IEnumerator Dofade()
+    {
+        CanvasGroup canvasGroup = this.GetComponent<CanvasGroup>();
+        canvasGroup.interactable = false;
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+        canvasGroup.interactable = true;
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/WinCondScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinCondScript : MonoBehaviour {
+
+    /*public Text gameOverText;
+    public Button startButton;
+    public Button retryButton;
+    public GameObject panel;*/
+
+    private bool WinGame;
+    private GameObject avalanche;
+    private bool playMusic;
+    private GameObject canvas;
+    public GameObject wintext;
+
+    private int count;
+    private int timer;
+    private string defaultText;
+    // Use this for initialization
+    private void Awake()
+    {
+        avalanche = GameObject.Find("Active Avalanche");
+    }
+
+    void Start () {
+        WinGame = false;
+        
+        playMusic = false;
+        //canvas = GameObject.Find("Canvas");
+        count = 10;
+        timer = 0;
+        defaultText = wintext.GetComponent<TextMesh>().text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (WinGame == true && timer % 18 == 0)
+        {
+            count--;
+        }
+        wintext.GetComponent<TextMesh>().text = defaultText + "\n" + "Restarting in " + count;
+        timer++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(avalanche.GetComponent<AvalancheController>().getEndAvalanche() && other.gameObject.name == "PlayerCollider")
+        {
+            WinGame = true;
+            if (!playMusic)
+            {
+                this.GetComponent<AudioSource>().Play(0);
+                wintext.SetActive(true);
+                /*gameOverText.text = "You Survived";
+                startButton.gameObject.SetActive(false);
+                FadeIn();*/
+                Invoke("RestartGame", 10);
+                playMusic = true;
+            }
+        }
+    }
+
+    public bool getWinCond()
+    {
+        return WinGame;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Temp");
+    }
+
+    /*void setRetryButton()
+    {
+        retryButton.gameObject.SetActive(true);
+        retryButton.interactable = true;
+    }
+
+
+    public void FadeIn()
+    {
+        StartCoroutine(Dofade());
+    }
+
+    IEnumerator Dofade()
+    {
+        CanvasGroup canvasGroup = canvas.GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha < 256)
+        {
+            canvasGroup.alpha += Time.deltaTime / 4;
+            yield return null;
+        }
+        yield return null;
+    }*/
+}
